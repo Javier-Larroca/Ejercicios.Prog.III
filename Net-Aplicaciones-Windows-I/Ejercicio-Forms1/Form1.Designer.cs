022dee5c53db587f5666c7534e165f2ed9a61288
Agus Larroca <javier-larroca@hotmail.com>
2021-04-08 00:40:15 -0300

    Resolucion de primeros ejercicios

---
+++ Net-Aplicaciones-Windows-I/Ejercicio-Forms1/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Ejercicio_Forms1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textApellido = new System.Windows.Forms.TextBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textEdad = new System.Windows.Forms.TextBox();
+            this.textDireccion = new System.Windows.Forms.TextBox();
+            this.textResultado = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "APELLIDO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NOMBRE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "EDAD";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "DIRECCIÓN";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "RESULTADO";
+            // 
+            // textApellido
+            // 
+            this.textApellido.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textApellido.Location = new System.Drawing.Point(100, 25);
+            this.textApellido.Name = "textApellido";
+            this.textApellido.Size = new System.Drawing.Size(262, 20);
+            this.textApellido.TabIndex = 5;
+            // 
+            // textNombre
+            // 
+            this.textNombre.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textNombre.Location = new System.Drawing.Point(100, 50);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(262, 20);
+            this.textNombre.TabIndex = 6;
+            // 
+            // textEdad
+            // 
+            this.textEdad.Location = new System.Drawing.Point(100, 75);
+            this.textEdad.Name = "textEdad";
+            this.textEdad.Size = new System.Drawing.Size(52, 20);
+            this.textEdad.TabIndex = 7;
+            this.textEdad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textDireccion
+            // 
+            this.textDireccion.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textDireccion.Location = new System.Drawing.Point(100, 100);
+            this.textDireccion.Name = "textDireccion";
+            this.textDireccion.Size = new System.Drawing.Size(262, 20);
+            this.textDireccion.TabIndex = 8;
+            // 
+            // textResultado
+            // 
+            this.textResultado.Location = new System.Drawing.Point(15, 173);
+            this.textResultado.Multiline = true;
+            this.textResultado.Name = "textResultado";
+            this.textResultado.Size = new System.Drawing.Size(411, 128);
+            this.textResultado.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(120, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "ACEPTAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(240, 327);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "CANCELAR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(447, 362);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textResultado);
+            this.Controls.Add(this.textDireccion);
+            this.Controls.Add(this.textEdad);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.textApellido);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datos personales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textApellido;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textEdad;
+        private System.Windows.Forms.TextBox textDireccion;
+        private System.Windows.Forms.TextBox textResultado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Net-Aplicaciones-Windows-I/Ejercicio-Forms1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_Forms1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 48 || e.KeyChar > 59) && e.KeyChar != 8)
+                e.Handled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Se reestablece el color de los textBox en caso de que ya esten en rojo.
+            textApellido.BackColor = System.Drawing.SystemColors.Control;
+            textNombre.BackColor = System.Drawing.SystemColors.Control;
+            textEdad.BackColor = System.Drawing.SystemColors.Control;
+            textDireccion.BackColor = System.Drawing.SystemColors.Control;
+
+            //Se pintan los textBox que no contengan contenido alguno.
+            if (textApellido.Text == "")
+                textApellido.BackColor = Color.Red;
+            if (textNombre.Text == "")
+                textNombre.BackColor = Color.Red;
+            if (textEdad.Text == "")
+                textEdad.BackColor = Color.Red;
+            if (textDireccion.Text == "")
+                textDireccion.BackColor = Color.Red;
+
+            //Se imprimen los datos en el cuadro de texto inferior.
+            if(!(textApellido.Text == "" || textNombre.Text == "" || textEdad.Text == "" || textDireccion.Text == ""))
+            {
+                textResultado.Text = "Apellido y nombre: " + textApellido + " " + textNombre;
+                textResultado.Text += "\r\n" + "Edad: " + textEdad;
+                textResultado.Text += "\r\n" + "Dirección: " + textDireccion;
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Se cierra la aplicacion.
+            this.Close();
+        }
+    }
+}
