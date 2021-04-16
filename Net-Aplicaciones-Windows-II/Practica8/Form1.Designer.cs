262f7d2a277869e456e99c1c3ec31cf184c37060
Agus Larroca <javier-larroca@hotmail.com>
2021-04-16 10:38:10 -0300

    Resolucion de practica 8

---
+++ Net-Aplicaciones-Windows-II/Practica8/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Practica8
+{
+    partial class frmListBoxCheckedListBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSeleccionados = new System.Windows.Forms.ListBox();
+            this.clbValores = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbSeleccionados
+            // 
+            this.lbSeleccionados.FormattingEnabled = true;
+            this.lbSeleccionados.Location = new System.Drawing.Point(473, 12);
+            this.lbSeleccionados.Name = "lbSeleccionados";
+            this.lbSeleccionados.Size = new System.Drawing.Size(291, 368);
+            this.lbSeleccionados.TabIndex = 0;
+            // 
+            // clbValores
+            // 
+            this.clbValores.CheckOnClick = true;
+            this.clbValores.FormattingEnabled = true;
+            this.clbValores.Items.AddRange(new object[] {
+            "Uno",
+            "Dos",
+            "Tres",
+            "Cuatro",
+            "Cinco",
+            "Seis",
+            "Siete",
+            "Ocho",
+            "Nueve"});
+            this.clbValores.Location = new System.Drawing.Point(13, 13);
+            this.clbValores.Name = "clbValores";
+            this.clbValores.Size = new System.Drawing.Size(294, 364);
+            this.clbValores.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(354, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Mover";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmListBoxCheckedListBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 391);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clbValores);
+            this.Controls.Add(this.lbSeleccionados);
+            this.Name = "frmListBoxCheckedListBox";
+            this.Text = "ListBox y CheckedListBox";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbSeleccionados;
+        private System.Windows.Forms.CheckedListBox clbValores;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
