e7e159c1ff8730de72330e434cdb54dba402d070
Agus Larroca <javier-larroca@hotmail.com>
2021-04-16 14:47:09 -0300

    Resolucion de PRactica 9

--- Net-Aplicaciones-Windows-III/Ejercitacion9/Form1.Designer.cs
+++ Net-Aplicaciones-Windows-III/Ejercitacion9/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Ejercitacion9
 {
-    partial class Form1
+    partial class formListView
     {
         /// <summary>
         /// Required designer variable.
@@ -30,29 +30,29 @@ namespace Ejercitacion9
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formListView));
+            this.lblActualdir = new System.Windows.Forms.Label();
             this.btnAtras = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioLI = new System.Windows.Forms.RadioButton();
-            this.radioSI = new System.Windows.Forms.RadioButton();
-            this.radioL = new System.Windows.Forms.RadioButton();
-            this.radioD = new System.Windows.Forms.RadioButton();
             this.radioT = new System.Windows.Forms.RadioButton();
+            this.radioD = new System.Windows.Forms.RadioButton();
+            this.radioL = new System.Windows.Forms.RadioButton();
+            this.radioSI = new System.Windows.Forms.RadioButton();
+            this.radioLI = new System.Windows.Forms.RadioButton();
             this.imageListSmall = new System.Windows.Forms.ImageList(this.components);
             this.imageListLarge = new System.Windows.Forms.ImageList(this.components);
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.lvCarpetas = new System.Windows.Forms.ListView();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // lblActualdir
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Directorio Actual";
+            this.lblActualdir.AutoSize = true;
+            this.lblActualdir.Location = new System.Drawing.Point(13, 13);
+            this.lblActualdir.Name = "lblActualdir";
+            this.lblActualdir.Size = new System.Drawing.Size(85, 13);
+            this.lblActualdir.TabIndex = 0;
+            this.lblActualdir.Text = "Directorio Actual";
             // 
             // btnAtras
             // 
@@ -62,6 +62,7 @@ namespace Ejercitacion9
             this.btnAtras.TabIndex = 2;
             this.btnAtras.Text = "Atras";
             this.btnAtras.UseVisualStyleBackColor = true;
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
             // 
             // groupBox1
             // 
@@ -79,35 +80,16 @@ namespace Ejercitacion9
             this.groupBox1.Text = "Modo de Vista";
             this.groupBox1.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.groupBox1_ControlAdded);
             // 
-            // radioLI
+            // radioT
             // 
-            this.radioLI.AutoSize = true;
-            this.radioLI.Location = new System.Drawing.Point(7, 30);
-            this.radioLI.Name = "radioLI";
-            this.radioLI.Size = new System.Drawing.Size(76, 17);
-            this.radioLI.TabIndex = 0;
-            this.radioLI.Text = "Large Icon";
-            this.radioLI.UseVisualStyleBackColor = true;
-            // 
-            // radioSI
-            // 
-            this.radioSI.AutoSize = true;
-            this.radioSI.Location = new System.Drawing.Point(7, 60);
-            this.radioSI.Name = "radioSI";
-            this.radioSI.Size = new System.Drawing.Size(74, 17);
-            this.radioSI.TabIndex = 1;
-            this.radioSI.Text = "Small Icon";
-            this.radioSI.UseVisualStyleBackColor = true;
-            // 
-            // radioL
-            // 
-            this.radioL.AutoSize = true;
-            this.radioL.Location = new System.Drawing.Point(7, 90);
-            this.radioL.Name = "radioL";
-            this.radioL.Size = new System.Drawing.Size(47, 17);
-            this.radioL.TabIndex = 2;
-            this.radioL.Text = "Lista";
-            this.radioL.UseVisualStyleBackColor = true;
+            this.radioT.AutoSize = true;
+            this.radioT.Location = new System.Drawing.Point(7, 150);
+            this.radioT.Name = "radioT";
+            this.radioT.Size = new System.Drawing.Size(51, 17);
+            this.radioT.TabIndex = 4;
+            this.radioT.Text = "Titulo";
+            this.radioT.UseVisualStyleBackColor = true;
+            this.radioT.CheckedChanged += new System.EventHandler(this.radioT_CheckedChanged);
             // 
             // radioD
             // 
@@ -120,16 +102,40 @@ namespace Ejercitacion9
             this.radioD.TabStop = true;
             this.radioD.Text = "Detalle";
             this.radioD.UseVisualStyleBackColor = true;
+            this.radioD.CheckedChanged += new System.EventHandler(this.radioD_CheckedChanged);
             // 
-            // radioT
+            // radioL
             // 
-            this.radioT.AutoSize = true;
-            this.radioT.Location = new System.Drawing.Point(7, 150);
-            this.radioT.Name = "radioT";
-            this.radioT.Size = new System.Drawing.Size(51, 17);
-            this.radioT.TabIndex = 4;
-            this.radioT.Text = "Titulo";
-            this.radioT.UseVisualStyleBackColor = true;
+            this.radioL.AutoSize = true;
+            this.radioL.Location = new System.Drawing.Point(7, 90);
+            this.radioL.Name = "radioL";
+            this.radioL.Size = new System.Drawing.Size(47, 17);
+            this.radioL.TabIndex = 2;
+            this.radioL.Text = "Lista";
+            this.radioL.UseVisualStyleBackColor = true;
+            this.radioL.CheckedChanged += new System.EventHandler(this.radioL_CheckedChanged);
+            // 
+            // radioSI
+            // 
+            this.radioSI.AutoSize = true;
+            this.radioSI.Location = new System.Drawing.Point(7, 60);
+            this.radioSI.Name = "radioSI";
+            this.radioSI.Size = new System.Drawing.Size(74, 17);
+            this.radioSI.TabIndex = 1;
+            this.radioSI.Text = "Small Icon";
+            this.radioSI.UseVisualStyleBackColor = true;
+            this.radioSI.CheckedChanged += new System.EventHandler(this.radioSI_CheckedChanged);
+            // 
+            // radioLI
+            // 
+            this.radioLI.AutoSize = true;
+            this.radioLI.Location = new System.Drawing.Point(7, 30);
+            this.radioLI.Name = "radioLI";
+            this.radioLI.Size = new System.Drawing.Size(76, 17);
+            this.radioLI.TabIndex = 0;
+            this.radioLI.Text = "Large Icon";
+            this.radioLI.UseVisualStyleBackColor = true;
+            this.radioLI.CheckedChanged += new System.EventHandler(this.radioLI_CheckedChanged);
             // 
             // imageListSmall
             // 
@@ -143,32 +149,33 @@ namespace Ejercitacion9
             // 
             this.imageListLarge.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListLarge.ImageStream")));
             this.imageListLarge.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageListLarge.Images.SetKeyName(0, "noun_Gears_2793720.png");
-            this.imageListLarge.Images.SetKeyName(1, "noun_Location_2793721.png");
-            this.imageListLarge.Images.SetKeyName(2, "noun_padlock_2793728.png");
+            this.imageListLarge.Images.SetKeyName(0, "noun_Location_2793721.png");
+            this.imageListLarge.Images.SetKeyName(1, "noun_padlock_2793728.png");
+            this.imageListLarge.Images.SetKeyName(2, "noun_Camera_2793724.png");
             // 
-            // listView1
+            // lvCarpetas
             // 
-            this.listView1.HideSelection = false;
-            this.listView1.LargeImageList = this.imageListLarge;
-            this.listView1.Location = new System.Drawing.Point(16, 39);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(429, 273);
-            this.listView1.SmallImageList = this.imageListSmall;
-            this.listView1.TabIndex = 4;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.lvCarpetas.HideSelection = false;
+            this.lvCarpetas.LargeImageList = this.imageListLarge;
+            this.lvCarpetas.Location = new System.Drawing.Point(16, 39);
+            this.lvCarpetas.Name = "lvCarpetas";
+            this.lvCarpetas.Size = new System.Drawing.Size(429, 273);
+            this.lvCarpetas.SmallImageList = this.imageListSmall;
+            this.lvCarpetas.TabIndex = 4;
+            this.lvCarpetas.UseCompatibleStateImageBehavior = false;
+            this.lvCarpetas.View = System.Windows.Forms.View.Details;
+            this.lvCarpetas.ItemActivate += new System.EventHandler(this.lvCarpetas_ItemActivate);
             // 
-            // Form1
+            // formListView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(654, 379);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lvCarpetas);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnAtras);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Controls.Add(this.lblActualdir);
+            this.Name = "formListView";
             this.Text = "ListView";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
@@ -179,7 +186,7 @@ namespace Ejercitacion9
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblActualdir;
         private System.Windows.Forms.Button btnAtras;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton radioT;
@@ -189,7 +196,7 @@ namespace Ejercitacion9
         private System.Windows.Forms.RadioButton radioLI;
         private System.Windows.Forms.ImageList imageListSmall;
         private System.Windows.Forms.ImageList imageListLarge;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView lvCarpetas;
     }
 }
 
--- Net-Aplicaciones-Windows-III/Ejercitacion9/Form1.cs
+++ Net-Aplicaciones-Windows-III/Ejercitacion9/Form1.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Ejercitacion9
 {
-    public partial class Form1 : Form
+    public partial class formListView : Form
     {
-        public Form1()
+        private System.Collections.Specialized.StringCollection carpetacol;
+        public formListView()
         {
             InitializeComponent();
+            carpetacol = new System.Collections.Specialized.StringCollection();
+            CrearCabecerayLlenarListView();
+            DibujarListView(@"C:\");
+            carpetacol.Add(@"C:\");
+
         }
 
         private void groupBox1_ControlAdded(object sender, ControlEventArgs e)
@@ -24,6 +31,134 @@ namespace Ejercitacion9
             groupBox1.Controls.Add(radioLI);
             groupBox1.Controls.Add(radioSI);
             groupBox1.Controls.Add(radioT);
+        }
+
+        private void CrearCabecerayLlenarListView()
+        {
+            ColumnHeader colCab; //Declaro un objeto ColumnHeader.
+            colCab = new ColumnHeader(); //Instancio el objeto ColumnHeader.
+            colCab.Text = "Nombre Archivo";
+            lvCarpetas.Columns.Add(colCab); // Inserto la Cabecera Nombre Archivo.
+            colCab = new ColumnHeader();
+            colCab.Text = "Tamaño";
+            lvCarpetas.Columns.Add(colCab); // Inserto la Cabecera Tamaño.
+            colCab = new ColumnHeader();
+            colCab.Text = "Ultima Modificacion";
+            lvCarpetas.Columns.Add(colCab); // Inserto la Cabecera Ultima Modificacion.
+        }
+
+        private void DibujarListView(string root)
+        {
+            try
+            {
+                ListViewItem lvi;
+                ListViewItem.ListViewSubItem lvsi;
+                if (string.IsNullOrEmpty(root))
+                    return;
+                DirectoryInfo dir = new DirectoryInfo(root);
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                FileInfo[] files = dir.GetFiles();
+                lvCarpetas.Items.Clear();
+                lblActualdir.Text = root;
+                lvCarpetas.BeginUpdate();
+                foreach (DirectoryInfo di in dirs)
+                {
+                    lvi = new ListViewItem();
+                    lvi.Text = di.Name;
+                    lvi.ImageIndex = 0;
+                    lvi.Tag = di.FullName;
+                    lvsi = new ListViewItem.ListViewSubItem();
+                    lvsi.Text = "";
+                    lvi.SubItems.Add(lvsi);
+                    lvsi = new ListViewItem.ListViewSubItem();
+                    lvsi.Text = di.LastAccessTime.ToString();
+                    lvi.SubItems.Add(lvsi);
+                    lvCarpetas.Items.Add(lvi);
+                }
+                foreach (FileInfo fi in files)
+                {
+                    lvi = new ListViewItem();
+                    lvi.Text = fi.Name;
+                    lvi.ImageIndex = 1;
+                    lvi.Tag = fi.FullName;
+                    lvsi = new ListViewItem.ListViewSubItem();
+                    lvsi.Text = fi.Length.ToString();
+                    lvi.SubItems.Add(lvsi);
+                    lvsi = new ListViewItem.ListViewSubItem(); lvsi.Text = fi.LastAccessTime.ToString();
+                    lvi.SubItems.Add(lvsi);
+                    lvCarpetas.Items.Add(lvi);
+                }
+                lvCarpetas.EndUpdate();
+            }
+            catch (System.Exception err)
+            {
+                MessageBox.Show("Error: " + err.Message);
+            }
+        }
+
+        private void lvCarpetas_ItemActivate(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ListView lw = (System.Windows.Forms.ListView)sender;
+            string filename = lw.SelectedItems[0].Tag.ToString();
+            if (lw.SelectedItems[0].ImageIndex != 0)
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start(filename);
+                }
+                catch { return; }
+            }
+            else
+            {
+                DibujarListView(filename);
+                carpetacol.Add(filename);
+            }
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            if (carpetacol.Count > 1)
+            {
+                DibujarListView(carpetacol[carpetacol.Count - 2].ToString());
+                carpetacol.RemoveAt(carpetacol.Count - 1);
+            }
+            else
+                DibujarListView(carpetacol[0].ToString());
+        }
+
+        private void radioLI_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rdb = (RadioButton)sender;
+            if (rdb.Checked)
+                this.lvCarpetas.View = View.LargeIcon;
+        }
+
+        private void radioSI_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rdb = (RadioButton)sender;
+            if (rdb.Checked)
+                lvCarpetas.View = View.SmallIcon;
+        }
+
+        private void radioL_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rdb = (RadioButton)sender;
+            if (rdb.Checked)
+                lvCarpetas.View = View.List;
+        }
+
+        private void radioD_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rdb = (RadioButton)sender;
+            if (rdb.Checked)
+                lvCarpetas.View = View.Details;
+        }
+
+        private void radioT_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rdb = (RadioButton)sender;
+            if (rdb.Checked)
+                lvCarpetas.View = View.Tile;
         }
     }
 }
